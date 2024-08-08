eb84a06babb7bb3df98242327288043e451cd662
Jishnu-21 <144204571+Jishnu-21@users.noreply.github.com>
2024-08-08 23:52:03 +0530

    Update Context.js

--- src/store/Context.js
+++ src/store/Context.js
@@ -1,20 +1,18 @@
 import { createContext, useContext, useState } from "react";
 
-export const FirebaseContext = createContext(null)
-
+export const FirebaseContext = createContext(null);
 export const AuthContext = createContext(null);
+export const ProductContext = createContext(null);
 
-export const ProductContext = createContext(null)
+export default function Context({ children }) {
+    const [user, setUser] = useState(null);
+    const [product, setProduct] = useState(null); // Create state for product
 
-
-export default function Comtext ({children}){
-    const [user,setUser] = useState(null)
-    const [product,productDetails] = useContext(ProductContext)
-    return(
-        <ProductContext.Provider value={{product,productDetails}}> 
-        <AuthContext.Provider value={{user,setUser}}>
-            {children}
-        </AuthContext.Provider>
+    return (
+        <ProductContext.Provider value={{ product, setProduct }}> 
+            <AuthContext.Provider value={{ user, setUser }}>
+                {children}
+            </AuthContext.Provider>
         </ProductContext.Provider>
-    )
+    );
 }
